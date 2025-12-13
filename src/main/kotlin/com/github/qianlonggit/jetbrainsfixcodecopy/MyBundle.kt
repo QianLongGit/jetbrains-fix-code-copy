b2c016a630977c6216e16ea0bf6c32ac265e306a
GitHub Action <action@github.com>
2025-12-13 05:55:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/qianlonggit/jetbrainsfixcodecopy/MyBundle.kt
+++ src/main/kotlin/com/github/qianlonggit/jetbrainsfixcodecopy/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.qianlonggit.jetbrainsfixcodecopy
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/qianlonggit/jetbrainsfixcodecopy/services/MyProjectService.kt
+++ src/main/kotlin/com/github/qianlonggit/jetbrainsfixcodecopy/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.qianlonggit.jetbrainsfixcodecopy.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.qianlonggit.jetbrainsfixcodecopy.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
